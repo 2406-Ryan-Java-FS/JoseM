116ece0ec5fed3dc59b37d511badc73fc3133cbb
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2024-06-26 12:09:30 -0400

    implement get all employees functionality

--- src/main/java/ems/employeemanagementsystem/controller/EmployeeController.java
+++ src/main/java/ems/employeemanagementsystem/controller/EmployeeController.java
@@ -1,8 +1,13 @@
 package ems.employeemanagementsystem.controller;
 
+import ems.employeemanagementsystem.entity.Employee;
+import ems.employeemanagementsystem.service.EmployeeService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -11,5 +16,10 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    
+    @RequestMapping
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+
+        return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
 }
--- src/main/java/ems/employeemanagementsystem/service/EmployeeService.java
+++ src/main/java/ems/employeemanagementsystem/service/EmployeeService.java
@@ -1,7 +1,19 @@
 package ems.employeemanagementsystem.service;
 
+import ems.employeemanagementsystem.entity.Employee;
+import ems.employeemanagementsystem.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@AllArgsConstructor
 @Service
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
 }
