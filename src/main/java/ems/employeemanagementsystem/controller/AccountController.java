bf7183c2c84b58e2765033b55474e88e554c6e5f
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2024-06-29 15:05:38 -0400

    add account controller file

---
+++ src/main/java/ems/employeemanagementsystem/controller/AccountController.java
@@ -0,0 +1,9 @@
+package ems.employeemanagementsystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+}
