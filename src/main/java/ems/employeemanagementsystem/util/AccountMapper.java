e213a61276f1460ae923b77a5d118ccc6d4aa4ee
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2024-06-29 15:29:34 -0400

    add utility mapper class

--- src/main/java/ems/employeemanagementsystem/util/AccountMapper.java
+++ src/main/java/ems/employeemanagementsystem/util/AccountMapper.java
@@ -1,2 +1,12 @@
-package ems.employeemanagementsystem.util;public class AccountMapper {
+package ems.employeemanagementsystem.util;
+
+import ems.employeemanagementsystem.entity.Account;
+import ems.employeemanagementsystem.entity.RegistrationRequest;
+import ems.employeemanagementsystem.role.Role;
+
+import java.util.ArrayList;
+
+public class AccountMapper {
+
+   
 }
