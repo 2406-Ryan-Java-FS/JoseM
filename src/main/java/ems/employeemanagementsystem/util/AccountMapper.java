64e37f2ba5d9037a1a95f56d6f46a0fab43487ee
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2024-06-29 15:30:13 -0400

    implement functionality to map from registration request to an account

--- src/main/java/ems/employeemanagementsystem/util/AccountMapper.java
+++ src/main/java/ems/employeemanagementsystem/util/AccountMapper.java
@@ -8,5 +8,13 @@ import java.util.ArrayList;
 
 public class AccountMapper {
 
-   
+    public static Account toAccount(RegistrationRequest request){
+
+        Account account = new Account();
+        account.setEmail(request.getEmail());
+        account.setPassword(request.getPassword());
+        account.setRole(Role.ROLE_USER);
+        account.setEmployees(new ArrayList<>());
+        return account;
+    }
 }
