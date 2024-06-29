f8597c7d866e88593afd3f5b7029ab00f1f34db6
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2024-06-29 15:22:15 -0400

    implement registration request class

---
+++ src/main/java/ems/employeemanagementsystem/entity/RegistrationRequest.java
@@ -0,0 +1,17 @@
+package ems.employeemanagementsystem.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class RegistrationRequest {
+
+    private String email;
+
+    private String password;
+}
---
+++ src/main/java/ems/employeemanagementsystem/util/AccountMapper.java
@@ -0,0 +1,2 @@
+package ems.employeemanagementsystem.util;public class AccountMapper {
+}
