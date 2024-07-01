e437cd30d831a44fcb0631b5f29d1986e37a3614
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2024-07-01 10:01:57 -0400

    add comment to the password encoder method

--- src/main/java/ems/employeemanagementsystem/configuration/SecurityConfiguration.java
+++ src/main/java/ems/employeemanagementsystem/configuration/SecurityConfiguration.java
@@ -1,7 +1,20 @@
 package ems.employeemanagementsystem.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration {
+
+    /**
+     * This method returns password encoder.
+     * 
+     * @return BCryptPasswordEncoder bean
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
