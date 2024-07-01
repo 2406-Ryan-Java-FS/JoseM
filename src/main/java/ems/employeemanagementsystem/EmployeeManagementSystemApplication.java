4cc2e5f4060fad84b1cf7df257d9cf17dbd6739f
Jose Marin Hernandez <josem.marinhernandez@gmail.com>
2024-07-01 10:26:39 -0400

    disable security auto configuration to be able to make requests to any endpoint

--- src/main/java/ems/employeemanagementsystem/EmployeeManagementSystemApplication.java
+++ src/main/java/ems/employeemanagementsystem/EmployeeManagementSystemApplication.java
@@ -2,8 +2,9 @@ package ems.employeemanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) // Disable security auto config
 public class EmployeeManagementSystemApplication {
 
     public static void main(String[] args) {
